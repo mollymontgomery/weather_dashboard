d75119d099bf2781cb4a1c60ebc2527985e49757
Molly Montgomery <Molly@mollys-air-2.lan>
2021-11-17 14:27:55 -0600

    started JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,15 @@
-var apiKey= "8f34a961cac45cf68d8dad2a485aae8b"
+function createCityList(citySearchList) {
+    $("#city-list").empty();
+}
 
+var keys = Object.keys(citySearchList);
+for (var i = 0; i <keys.length; i++){
+    var cityListSpot = $("<button>");
+    cityListSpot.addClass("list-group-item list-group-item-action");
+}
+
+
+var apiKey= "8f34a961cac45cf68d8dad2a485aae8b"
 
 var response =fetch("api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}");
 console.log(response)
