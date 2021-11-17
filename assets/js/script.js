bbb9180989ae070b184216154a4878f3b8773dcc
Molly Montgomery <Molly@mollys-air-2.lan>
2021-11-17 15:39:33 -0600

    updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,9 @@
-function createCityList(citySearchList) {
-    $("#city-list").empty();
-}
 
-var keys = Object.keys(citySearchList);
-for (var i = 0; i <keys.length; i++){
-    var cityListSpot = $("<button>");
-    cityListSpot.addClass("list-group-item list-group-item-action");
-}
-
-
+// My weather API key
 var apiKey= "8f34a961cac45cf68d8dad2a485aae8b"
 
-var response =fetch("api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}");
-console.log(response)
+var searchCityFormEl = document.querySelector("#form-inline")
+
+var searchCity = function(city) {
+    var apiUrl = api.openweathermap.org/data/2.5/weather?q= {searchCity} &appid= + apiKey + "&units=imperial";
+}
