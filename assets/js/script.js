84eb8c1d17e4fc0f0800ac35325f012e31f4d0c8
Molly Montgomery <Molly@mollys-air-2.lan>
2021-11-18 20:14:35 -0600

    updates to index

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 // My weather API key
 var apiKey= "8f34a961cac45cf68d8dad2a485aae8b"
-var searchQueryURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+ apiKey; 
+//var searchQueryURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+ apiKey; 
+
+var searchedCities = JSON.parse(localStorage.getItem("city")) || [];
+var searchEl = document.getElementById("search-button");
+
 
 fetch(searchQueryURL).then(function(response){
     if (response.ok){
