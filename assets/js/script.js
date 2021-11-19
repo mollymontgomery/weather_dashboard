7db31a352a908b94854f98d0b6444902655f639e
Molly Montgomery <Molly@mollys-air-2.lan>
2021-11-18 20:16:34 -0600

    getting button to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,14 +4,22 @@ var apiKey= "8f34a961cac45cf68d8dad2a485aae8b"
 
 var searchedCities = JSON.parse(localStorage.getItem("city")) || [];
 var searchEl = document.getElementById("search-button");
+var cityEl = document.getElementById("city");
+var weatherDiv = document.getElementById("current-weather");
+var cityHistory = document.getElementById("searchedCities");
+var weatherContainer = document.getElementById("weather-conditions");
+var fiveDayDiv = document.getElementById("fiveday-container")
 
+// event for city search
+searchEl.addEventListener("click", function() {
+    var inputEl = document.getElementById("city-input");
+    console.log(inputEl.value); 
+    searchedCities.push(inputEl.value);
+    
+    localStorage.setItem("city", JSON.stringify(searchedCities));
 
-fetch(searchQueryURL).then(function(response){
-    if (response.ok){
-        return response.json()
-    }
+    getWeather(inputEl.value);
+    renderButton(inputEl.value);
 })
-.then(function(data){
-    console.log("city, data")
-})
+
 
